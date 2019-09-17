94bd41185c0bcbba7a1426343fe2066ec710619e
Roman Efimov <31079452+romarioe@users.noreply.github.com>
2019-09-17 20:03:33 +0300

    Initial Commit

---
+++ KubanTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KubanTestProject
+//
+//  Created by Roman Efimov on 17/09/2019.
+//  Copyright © 2019 Roman Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
